d815dd02b5c1f7ed2ee33460633adc9d93b759f7
Dylan Peerenboom & Randy Leighton <student@epicodus.com>
2014-07-21 12:06:15 -0700

    html updated, jquery integrated, incomplete

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,8 @@ var Tamagotchi = {
   activity: function(){
     this.activityLevel = this.activityLevel + 1;
   },
-  initialize: function(name) {
-    this.name = name;
+  initialize: function(tName) {
+    this.tName = tName;
     this.foodLevel = 10;
     this.sleepLevel = 10;
     this.activityLevel = 10;
@@ -23,7 +23,6 @@ var Tamagotchi = {
     this.timePasses();
     this.alive();
   },
-
   alive: function() {
     if ((this.foodLevel <= 0) || (this.sleepLevel <= 0) ||
       (this.activityLevel <= 0)) {
@@ -35,3 +34,16 @@ var Tamagotchi = {
     }
   }
 }
+
+$(document).ready(function(){
+  $('form#tamagotchiGame').submit(function(event){
+    event.preventDefault();
+    var myPet = Object.create(Tamagotchi);
+    var petName = $('#petName').val();
+    myPet.initialize(petName);
+    $("#output-name").text(myPet.tName);
+    $("#output-food").text(myPet.foodLevel);
+    $("#output-sleep").text(myPet.sleepLevel);
+    $("#output-activity").text(myPet.activityLevel);
+  })
+})
